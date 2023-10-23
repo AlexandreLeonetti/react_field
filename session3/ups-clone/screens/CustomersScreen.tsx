be66f3a4c4de0b55a9f4d460021301593a7820d6
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-23 17:14:30 +0200

    expo4

---
+++ session3/ups-clone/screens/CustomersScreen.tsx
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView} from "react-native"
+import React from "react"
+import { useTailwind } from "tailwind-rn";
+
+const CustomersScreen = () => {
+    const tw = useTailwind();
+
+    return(
+        <SafeAreaView>
+            <Text style={tw('text-red-500')}>Customer Screen</Text>
+        </SafeAreaView>
+    )
+}
+
+
+export default CustomersScreen;
