2b7b470f34c4b8c121604ece74c87ddf541db087
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-24 20:04:32 +0200

    graphQl

--- session3/ups-clone/screens/CustomersScreen.tsx
+++ session3/ups-clone/screens/CustomersScreen.tsx
@@ -1,14 +1,48 @@
-import { View, Text, SafeAreaView} from "react-native"
-import React from "react"
+import { View, Text, SafeAreaView, ScrollView, ActivityIndicator} from "react-native"
+import React, {useLayoutEffect, useState} from "react"
 import { useTailwind } from "tailwind-rn";
+import {CompositeNavigationProp, useNavigation} from "@react-navigation/native";
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs"  ;
+import {TabStackParamList} from "../navigator/TabNavigator";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import {Image, Input} from "@rneui/themed"
+
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+                                                                    BottomTabNavigationProp<TabStackParamList, "Customers">,
+                                                                    NativeStackNavigationProp<RootStackParamList>
+                                                                >
+
+
 
 const CustomersScreen = () => {
     const tw = useTailwind();
+    const navigation = useNavigation()<CustomerScreenNavigationProp>;
+//<any>//<CustomerScreenNavigationProp>;
+    const [input, setInput] = useState<string>("")
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerShown:false,
+        });
+    }, []);
 
     return(
-        <SafeAreaView>
-            <Text style={tw('text-red-500')}>Customer Screen</Text>
-        </SafeAreaView>
+        <ScrollView style = {{backgroundColor : "#59c1cc"}}>
+            <Image source  = {{ uri:"https://links.papareact.com/3jc"}}
+                    containerStyle={tw("w-full h-64")}
+                    PlaceholderContent={<ActivityIndicator/>}
+            />
+
+            <Input 
+                placeholder= "search by customer"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                containerStyle={tw("bg-white pt-5 pb-0  px-10")}
+            />
+
+        </ScrollView>
     )
 }
 
