b3517464c815889d80bfcebf6e361c31541be89d
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-15 18:52:39 +0100

    o

---
+++ node/proto/index.js
@@ -0,0 +1,21 @@
+//https://www.youtube.com/watch?v=01jVgCK-HX4
+//https://www.youtube.com/watch?v=mQ4oCgcgHOA
+let log = console.log;
+
+let o1 = {}; // new empty obj
+
+log(o1);
+log(o1.constructor);
+log(o1.__proto__);
+
+
+function Cat () {
+    // cat constructor
+}
+
+
+let kitty = new Cat();
+
+
+
+
