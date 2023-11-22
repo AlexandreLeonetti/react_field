9df8644a5d5e4c10cfeba2867d384cb638e34d6c
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 09:22:17 +0100

    ev

---
+++ js_prep/eventLoop.js
@@ -0,0 +1,12 @@
+/* some event loop demo */
+
+setTimeout(() => console.log(1), 0);
+
+console.log(2);
+
+new Promise(res => {
+    console.log(3);
+    res();
+}).then(()=> console.log(4));
+
+console.log(5);
