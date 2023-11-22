f54ebd7963eaa4b0eefe07dde02bc09af6510388
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 18:05:35 +0100

    promises2

--- js_prep/promises.js
+++ js_prep/promises.js
@@ -1,5 +1,13 @@
 /* some promises pattern */
 
-Promise.resolve(1);
+Promise.resolve(10)
+.then(x => { throw x })
+.then(x => console.log(`then ${x}`))
+.catch(err => console.log(`error ${err}`))
+.then(()=>Promise.resolve(2))
+.catch(err => console.log( `error ${err}`))
+.then(x => console.log(` then ${x}`));
+
+
 
 
