188acf99345ce71c0692eb85eebbbe4144d65817
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 14:27:54 +0100

    bar

--- js_prep/varScope.js
+++ js_prep/varScope.js
@@ -1,3 +1,23 @@
 /* some variable scope test */
 
+//console.log(x);//undefined
+//console.log(y);//referenceError cannot access y before initialization
+//console.log(z);//referenceError cannot access z before initialization
+"use strict";
 
+let w = 11;
+const x = 12;
+
+console.log(foo()); // undefined if foo does not return anything.
+
+let bar = "bar";
+
+var v = 10;
+
+function foo(){
+    //return bar; // throw error : cannot access bar before initialization.
+    return v;// return undefined 
+}
+
+
+bar = "baz";
