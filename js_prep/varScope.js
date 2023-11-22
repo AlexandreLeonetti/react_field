11fa1a238dd7642e46f3b0ef2f243d67eb54d47f
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 14:31:24 +0100

    bar2

--- js_prep/varScope.js
+++ js_prep/varScope.js
@@ -4,20 +4,29 @@
 //console.log(y);//referenceError cannot access y before initialization
 //console.log(z);//referenceError cannot access z before initialization
 "use strict";
-
+//let bar;
 let w = 11;
+let z;
 const x = 12;
 
 console.log(foo()); // undefined if foo does not return anything.
+// let bar should be above. cannot access before initialization.
+// and especially before declaration.
 
-let bar = "bar";
+ let bar = "bar";
 
 var v = 10;
 
 function foo(){
-    //return bar; // throw error : cannot access bar before initialization.
-    return v;// return undefined 
+    return bar; // throw error : cannot access bar before initialization.
+    //return v;// return undefined 
 }
 
 
 bar = "baz";
+
+
+/* commentary :
+ * the place before the let/const variable definition is called the dead zone.
+ * if we try to access them defore their definition, it will throw an error.
+ * */
