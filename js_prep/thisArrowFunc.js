5787a747db76086fc8444c930872f4dd2a811bf5
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 11:12:33 +0100

    this2

---
+++ js_prep/thisArrowFunc.js
@@ -0,0 +1,23 @@
+/* the value of this in arrow functions */
+
+"use strict";
+
+var x = 5;
+var y = 5;
+
+
+function Operations( op1 = x, op2 = y){
+    this.x = op1;
+    this.y = op2;
+};
+
+
+Operations.prototype.sum = () => this.x + this.y;
+
+const op = new Operations(10, 20);
+
+console.log(op.sum());
+
+
+
+
