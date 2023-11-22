afad8f57b6048c98c88ea47db18fc229c8197486
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 14:35:29 +0100

    foo

--- js_prep/varScope.js
+++ js_prep/varScope.js
@@ -29,4 +29,10 @@ bar = "baz";
 /* commentary :
  * the place before the let/const variable definition is called the dead zone.
  * if we try to access them defore their definition, it will throw an error.
+ * This behavior was chosen because of the const variables.
+ * when accessing var before its declaration, it would return undefined.
+ * this would be unnacceptable for const as it would no longer be considered constant.
+ *
+ * because the function call to foo is happening before declaration of bar,
+ * the variable is being called in the dead zone. which is why it throws an error.
  * */
