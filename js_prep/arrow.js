a01f1660d7a995161705759ddae8baf7804d6aae
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-11-22 12:16:03 +0100

    test

--- js_prep/arrow.js
+++ js_prep/arrow.js
@@ -1,3 +1,14 @@
 /* arrow functions don't have a prototype
  * arrow functions can't be used as constructor
  */
+
+
+const Num = () => {
+    this.getNum = () => 10 ;// this does not work !!
+}
+
+Num.prototype.getNum = () => 20;// returns typeError, no proto on arrow func !
+
+const num = new Num();
+
+console.log(num.getNum);
