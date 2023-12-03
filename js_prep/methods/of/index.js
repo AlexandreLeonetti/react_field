7aaa73ca114c477666a54c7bb0f8db2a064419ce
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-12-03 14:53:53 +0100

    Array.of

---
+++ js_prep/methods/of/index.js
@@ -0,0 +1,8 @@
+/* Array.of() static method */
+/* turns args into elements of array */
+
+console.log(Array.of(7));
+console.log(Array(7));
+console.log(Array(undefined));
+console.log(Array.of(1,2,3,4));
+
