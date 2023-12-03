4789c44ef26c038db44fc45ee5271d5dfdaaa5b7
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-12-03 10:52:00 +0100

    filter

--- js_prep/methods/fill/index.js
+++ js_prep/methods/fill/index.js
@@ -1,6 +1,6 @@
 const array1 = [1, 2, 3, 4];
 
- array1.fill(0,2,10);
+array1.fill(0,2,10);
 console.log(array1);
 array1.fill(8);
 console.log(array1);
---
+++ js_prep/methods/filter/index.js
@@ -0,0 +1,4 @@
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const res = words.filter((word) => { return (word.length > 6)});
+console.log(res);
