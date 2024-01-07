5b295a7ec07c82ff76137c00eda3750cc6c36a51
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-01-07 18:48:34 +0100

    modules

--- js_prep/2024/es6-modules/esm1/index.js
+++ js_prep/2024/es6-modules/esm1/index.js
@@ -1,31 +1,6 @@
-// logger.js
-
-//exports  a function as log
-export function log (message){
-    console.log(message);
-}
-
-/* export a constant as default level */
-export const DEFAULT_LEVEL = "info";
+//import * as lmod from "./mod.js";
+import {log} from "./mod.js";
 
 
-/* exports an object */
-export const levels = {
-    error : 0,
-    debug : 1,
-    warn  : 2
-}
-
-
-export class Logger {
-    constructor(name){
-        this.name = name;
-    }
-
-    log(message){
-        console.log(`[${this.message}]  ${message}`);
-    }
-}
-
-
-
+log("hello");
+//console.log(lmod);
