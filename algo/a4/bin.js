89ce0238d61615bba036e09b40a23aea717af67d
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-01-30 11:01:43 +0100

    bin

--- algo/a4/bin.js
+++ algo/a4/bin.js
@@ -1,1 +1,23 @@
 /* compute binary parity of some integer */
+
+
+let a = 1025; // 2 power 10
+
+function parity(x){
+    let par = 0;
+
+    while(x>0){
+        let i = x&1
+        par = i^par;
+        x = x>>1;
+    } 
+
+
+    return par; // returns true if number of 1 is even
+}
+
+console.log(parity(a));
+
+
+
+
