b23c5dbce836225573cb57b2e4c1603d15223e0f
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-02-02 13:20:32 +0100

    new

--- algo/a6/index.js
+++ algo/a6/index.js
@@ -10,45 +10,6 @@ var array4 = [-2, -1, 0, 3, 15];
 
 
 function merge(arrs){
-    /* generate array of iterators for each arr */
-    let iters = arrs.map((x) => x[Symbol.iterator]()); 
-    let heap  = [];
-    let res   = [];
-
-    /* initialise the heap with each first element from arrays */
-
-
-    iters.forEach((iter, idx)=>{
-        /* push [val idx] into heap */
-        let first = iter.next();
-
-        heap.push([ first.value, idx ]);
-
-        /* sort heap */
-        heap.sort((a,b)=>{return a[0]-b[0];});
-
-    });
-
-    /* push other elements into the heap */
-    while(heap.length>0){
-        
-        /* extract smallest element and index from heap */
-        let[ smallestE, smallestIdx ]= heap.shift();
-
-        /* push in heap the next elt that comes from the same iterator as smallest */
-        res.push(smallestE);
-
-        /* call next element of idx iterator iters[smallestIdx] */
-        let nextE = iters[smallestIdx].next().value;
-        if (nextE !== undefined){
-            heap.push([nextE, smallestIdx]);
-            heap.sort((a,b)=>a[0]-b[0]);
-        }
-    }
-
-
-    return res;
-
 
 }
 
